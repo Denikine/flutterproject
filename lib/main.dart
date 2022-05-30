4d2c59b26fb2bc745a2022834f80c8953c5dd545
Denikine <tug971@gmail.com>
2022-05-30 13:59:15 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,44 +9,44 @@ import 'RemindMe/Data.dart';
 import 'RemindMe/Notfound.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Navigation avec Flutter',
-    initialRoute: Accueil.routeName,
-    routes: {
-      Accueil.routeName : (context) => const Accueil(),
-    },
-    
-    onGenerateRoute: (settings) {
-      if (settings.name == Inscrire.routeName) {
-        final Data arguments = settings.arguments as Data;
-        return MaterialPageRoute(builder: (context) {
-          return Inscrire(
-            title: arguments.title,
-            content: arguments.content,
-          );
-        });
-      }
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Navigation avec Flutter',
+      initialRoute: Accueil.routeName,
+      routes: {
+        Accueil.routeName: (context) => const Accueil(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == Inscrire.routeName) {
+          final Data arguments = settings.arguments as Data;
+          return MaterialPageRoute(builder: (context) {
+            return Inscrire(
+              title: arguments.title,
+              content: arguments.content,
+            );
+          });
+        }
 
-      if (settings.name == Seconnecter.routeName) {
-        final Data arguments = settings.arguments as Data;
-        return MaterialPageRoute(builder: (context) {
-          return Seconnecter(
-            title: arguments.title,
-            content: arguments.content,
-          );
-        });
-      }
+        if (settings.name == Seconnecter.routeName) {
+          final Data arguments = settings.arguments as Data;
+          return MaterialPageRoute(builder: (context) {
+            return Seconnecter(
+              title: arguments.title,
+              content: arguments.content,
+            );
+          });
+        }
 
-      if (settings.name == Principale.routeName) {
-        final Data arguments = settings.arguments as Data;
-        return MaterialPageRoute(builder: (context) {
-          return Principale(
-            title: arguments.title,
-            content: arguments.content,
-          );
-        });
-      }
+        if (settings.name == Principale.routeName) {
+          final Data arguments = settings.arguments as Data;
+          return MaterialPageRoute(builder: (context) {
+            return Principale(
+              title: arguments.title,
+              content: arguments.content,
+            );
+          });
+        }
         if (settings.name == CreerRappel.routeName) {
           final Data arguments = settings.arguments as Data;
           return MaterialPageRoute(builder: (context) {
@@ -57,27 +57,20 @@ void main() {
             );
           });
         }
+
+        if (settings.name == Parametres.routeName) {
+          final Data arguments = settings.arguments as Data;
+          return MaterialPageRoute(builder: (context) {
+            return Parametres(
+              title: arguments.title,
+              content: arguments.content,
+            );
+          });
+        }
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(builder: (context) => NotFound());
       },
-      if (settings.name == Parametres.routeName) {
-        final Data arguments = settings.arguments as Data;
-        return MaterialPageRoute(builder: (context) {
-          return Parametres(
-            title: arguments.title,
-            content: arguments.content,
-          );
-        });
-      }
-
-    },
-
-    onUnknownRoute: (settings) {
-      return MaterialPageRoute(builder: (context) => NotFound());
-    },
-
-  ),);
+    ),
+  );
 }
-
-
