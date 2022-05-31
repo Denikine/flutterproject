0e8abc0c641a4c270e5765380b2ed117182dfd89
Denikine <tug971@gmail.com>
2022-05-31 16:35:59 +0200

    Update create_reminder.dart

--- lib/ui/create_reminder.dart
+++ lib/ui/create_reminder.dart
@@ -85,55 +85,58 @@ class _TextFieldDateTimePickerState extends State<TextFieldDateTimePicker> {
                 const LabelText(
                   labelValue: "Date et heure",
                 ),
-                TextfieldDateAndTimePicker(
-                  cupertinoDatePickerBackgroundColor: Colors.white,
-                  cupertinoDatePickerMaximumDate: DateTime(2099),
-                  cupertinoDatePickerMaximumYear: 2099,
-                  cupertinoDatePickerMinimumYear: 1990,
-                  cupertinoDatePickerMinimumDate: DateTime(1990),
-                  cupertinoDateInitialDateTime: DateTime.now(),
-                  materialDatePickerFirstDate: DateTime.now(),
-                  materialDatePickerInitialDate: DateTime.now(),
-                  materialDatePickerLastDate: DateTime(2099),
-                  preferredDateFormat: DateFormat.yMMMEd(),
-                  materialTimePickerUse24hrFormat: false,
-                  cupertinoTimePickerMinuteInterval: 1,
-                  cupertinoTimePickerUse24hFormat: false,
-                  textfieldDateAndTimePickerController: _controller,
-                  style: TextStyle(
-                    fontSize: displayWidth(context) * 0.040,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.black,
-                  ),
-                  textCapitalization: TextCapitalization.sentences,
-                  cursorColor: Colors.black,
-                  decoration: InputDecoration(
-                    //errorText: errorTextValue,
-                    helperStyle: TextStyle(
-                        fontSize: displayWidth(context) * 0.031,
-                        fontWeight: FontWeight.w700,
-                        color: Colors.grey),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide:
-                            const BorderSide(color: Colors.white, width: 0),
-                        borderRadius: BorderRadius.circular(2)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(2),
-                        borderSide: const BorderSide(
-                          width: 0,
-                          color: Colors.white,
-                        )),
-                    hintText: 'Sélectionner la date',
-                    hintStyle: TextStyle(
-                        fontSize: displayWidth(context) * 0.04,
-                        fontWeight: FontWeight.normal),
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+                Container(
+                  width: 320,
+                  child: TextfieldDateAndTimePicker(
+                    cupertinoDatePickerBackgroundColor: Colors.white,
+                    cupertinoDatePickerMaximumDate: DateTime(2099),
+                    cupertinoDatePickerMaximumYear: 2099,
+                    cupertinoDatePickerMinimumYear: 1990,
+                    cupertinoDatePickerMinimumDate: DateTime(1990),
+                    cupertinoDateInitialDateTime: DateTime.now(),
+                    materialDatePickerFirstDate: DateTime.now(),
+                    materialDatePickerInitialDate: DateTime.now(),
+                    materialDatePickerLastDate: DateTime(2099),
+                    preferredDateFormat: DateFormat.yMMMEd(),
+                    materialTimePickerUse24hrFormat: false,
+                    cupertinoTimePickerMinuteInterval: 1,
+                    cupertinoTimePickerUse24hFormat: false,
+                    textfieldDateAndTimePickerController: _controller,
+                    style: TextStyle(
+                      fontSize: displayWidth(context) * 0.040,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
                     ),
+                    textCapitalization: TextCapitalization.sentences,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      //errorText: errorTextValue,
+                      helperStyle: TextStyle(
+                          fontSize: displayWidth(context) * 0.031,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.grey),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 0),
+                          borderRadius: BorderRadius.circular(2)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(2),
+                          borderSide: const BorderSide(
+                            width: 0,
+                            color: Colors.white,
+                          )),
+                      hintText: 'Sélectionner la date',
+                      hintStyle: TextStyle(
+                          fontSize: displayWidth(context) * 0.03,
+                          fontWeight: FontWeight.normal),
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    materialInitialTime: TimeOfDay.now(),
                   ),
-                  materialInitialTime: TimeOfDay.now(),
                 ),
                 const LabelText(labelValue: "Commentaire"),
                 const SizedBox(
